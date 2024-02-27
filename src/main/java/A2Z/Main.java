c7fa7bd83efb398ab09bd59de6baf41949428cc8
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2024-02-28 00:47:59 +0530

    initial commit

---
+++ src/main/java/A2Z/Main.java
@@ -0,0 +1,7 @@
+package A2Z;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/A2Z/_01_Basics/_01_things_to_know/_01_user_ip_op.java
@@ -0,0 +1,7 @@
+package A2Z._01_Basics._01_things_to_know;
+
+public class _01_user_ip_op {
+    public static void main(String[] args) {
+
+    }
+}
