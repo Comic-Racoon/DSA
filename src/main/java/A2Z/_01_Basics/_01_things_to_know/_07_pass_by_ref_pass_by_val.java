b1a6a0f4a63f1cc1a1ba68d5138cd00a02bdc965
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2024-03-01 00:39:40 +0530

    added java pass by ref and pass by value theory

---
+++ src/main/java/A2Z/_01_Basics/_01_things_to_know/_07_pass_by_ref_pass_by_val.java
@@ -0,0 +1,20 @@
+package A2Z._01_Basics._01_things_to_know;
+
+public class _07_pass_by_ref_pass_by_val {
+    /*
+Q~ Is Java Pass-By-Reference or Pass-By-Value?
+A~   A simple Google search of this question will return a straightforward answer that Java is always pass-by-value.
+    The reason this question is confusing is because Java achieves its functionality using special variable types known
+    as object references.
+    --->
+    Pass-by-reference:
+        When a method is called, the method arguments reference the same variable in memory as the caller.
+    Pass-by-value:
+        When a method is called, the caller passes a copy of the argument variables to the method resulting in two
+        values in memory.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Refer this later : https://sentry.io/answers/java-pass-by-reference-or-value/#:~:text=A%20simple%20Google%20search%20of,types%20known%20as%20object%20references.");
+    }
+}
