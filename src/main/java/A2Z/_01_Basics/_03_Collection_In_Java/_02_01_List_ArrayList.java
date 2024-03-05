f9e9bb6e7d363966faa0b7155f863f2d658181d0
Rahul Umesh Mhatre <rahul.mhatre@phenompeople.com>
2024-03-06 00:34:41 +0530

    added array list points

---
+++ src/main/java/A2Z/_01_Basics/_03_Collection_In_Java/_02_01_List_ArrayList.java
@@ -0,0 +1,35 @@
+package A2Z._01_Basics._03_Collection_In_Java;
+
+public class _02_01_List_ArrayList {
+
+/*
+    The Important Points About ArrayList are :
+        * ArrayList class are resizeable array or growable array
+        * ArrayList class can contain duplicate elements.
+        * ArrayList class maintains insertion order.
+        * ArrayList class is "not" synchronized.
+        * ArrayList class allows random access as it works on index basis.
+        * ArrayList class allows heterogeneous object.
+        * Null Insertion is Possible.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+}
